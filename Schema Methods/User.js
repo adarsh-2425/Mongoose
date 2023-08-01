969341bb103991da850b8a1a6d004851c705bfc9
Adarsh S <102573286+adarsh-2425@users.noreply.github.com>
2023-08-01 08:34:33 +0530

    Create User.js

---
+++ Schema Methods/User.js
@@ -0,0 +1,39 @@
+const mongoose =  require('mongoose');
+
+//schema for address
+const addressSchema = new mongoose.Schema({
+  street: String,
+  city: String
+});
+
+//create user schema
+const UserSchema = new mongoose.Schema({
+  name: String,
+  age: {
+    type: Number,
+    min: 1
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true
+  },
+  createdAt: {
+    type: Date,
+    immutable: true,
+    default: () => Date.now()
+  },
+  updatedAt: Date,
+  bestFriend: mongoose.SchemaTypes.ObjectId,
+  hobbies: [String],
+  address: addressSchema
+});
+
+//SCHEMA METHOD HERE!!!!!!!!!!
+//custom method named sayHi()
+UserSchema.methods.sayHi = function() {
+  console.log(`Hi, My name is ${this.name}`);
+};
+
+//user model
+module.exports = mongoose.model('User', UserSchema);
